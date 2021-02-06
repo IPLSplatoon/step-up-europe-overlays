8718a422a073b3b51c7b74332b27cb8d68e06c2d
Vincent Lee <vlee@vlee.me.uk>
2021-02-06 16:12:27 +0000

    remove redundant code

--- graphics/scripts/break/break.js
+++ graphics/scripts/break/break.js
@@ -11,10 +11,6 @@ function setMainSceneText(textElemID, newText, tl, oldText = '') {
 	let textElem = document.querySelector(`#${textElemID}`);
 	if (textElem.getAttribute('text') === newText) return;
 
-	// if (oldText === '') {
-	// 	oldText = textElem.getAttribute('text');
-	// }
-
 	tl.add(gsap.fromTo(textElem, {clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)'}, {
 		ease: 'power2.inOut', duration: 1, clipPath: 'polygon(0 0, 0% 0, 0% 100%, 0% 100%)', onComplete: function () {
 			textElem.setAttribute('text', newText);
