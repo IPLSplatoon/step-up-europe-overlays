f2a1d0cf07e465855fc52d2b916423054ebfdc95
Vincent Lee <vlee@vlee.me.uk>
2021-02-10 14:01:37 +0000

    Change pin size and placement

--- graphics/scripts/stageInfo.js
+++ graphics/scripts/stageInfo.js
@@ -25,19 +25,3 @@ const mapNameToImagePath = {
 	"Skipper Pavilion": "stages/S2_Stage_Skipper_Pavilion.png",
 	"Unknown Map": "stages/low-ink-unknown-map.png"
 };
-
-// // Replicants
-// const bigTextValue = nodecg.Replicant('mainFlavorText', 'ipl-overlay-controls');
-// const casterNames = nodecg.Replicant('casterNames', 'ipl-overlay-controls');
-// const nowPlaying = nodecg.Replicant('nowPlaying', 'ipl-overlay-controls');
-// const nowPlayingManual = nodecg.Replicant('nowPlayingManual', 'ipl-overlay-controls');
-// const mSongEnabled = nodecg.Replicant('mSongEnabled', 'ipl-overlay-controls');
-// const musicShown = nodecg.Replicant('musicShown', 'ipl-overlay-controls');
-// const currentBreakScene = nodecg.Replicant('currentBreakScene', 'ipl-overlay-controls');
-// const nextTeams = nodecg.Replicant('nextTeams', 'ipl-overlay-controls');
-// const currentMaplistID = nodecg.Replicant('currentMaplistID', 'ipl-overlay-controls');
-// const mapWinners = nodecg.Replicant('mapWinners', 'ipl-overlay-controls');
-// const SBData = nodecg.Replicant('SBData', 'ipl-overlay-controls');
-// const NSTimerShown = nodecg.Replicant('NSTimerShown', 'ipl-overlay-controls');
-// const nextStageTime = nodecg.Replicant('nextStageTime', 'ipl-overlay-controls');
-// const maplists = nodecg.Replicant('maplists', 'ipl-overlay-controls');
